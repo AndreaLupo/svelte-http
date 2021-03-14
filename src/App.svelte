08558b018c21c9b885b5d2a156b13ce93d09f4b8
Wolf Developer <lupo.andrea.93@gmail.com>
2021-03-14 12:47:05 +0100

    Add data

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,36 @@
 <script>
-	export let name;
+
+
+	let hobbies = [];
+	let hobbyInput;
+
+	function addHobby() {
+		hobbies = [...hobbies, hobbyInput.value];
+
+		fetch('https://svelte-course-a2f4f-default-rtdb.firebaseio.com/hobbies.json', 
+			{
+				method: 'POST',
+				body: JSON.stringify(hobbies),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		).then(res => {
+			if(!res.ok) {
+				throw new Error("Faild!");
+			}
+		}).catch(err => {
+			console.log(err);
+		});
+	}
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput}>
+<button on:click={addHobby}>Add hobby</button>
 
-<h1>Hello {name}!</h1>
+<ul>
+	{#each hobbies as hobby}
+		<li>{hobby}</li>
+	{/each}
+</ul>
